4f8b2dd3149730461b8568d7c3debef95c3e09fa
Emirhan Karakadıoğlu <70448242+Bolzac@users.noreply.github.com>
2022-09-08 13:52:15 +0300

    Add files via upload

---
+++ While_loop/Program.cs
@@ -0,0 +1,28 @@
+﻿class Function
+{
+    public static void Limit(int initial, int limit)
+    {
+        int sum = initial;
+        Console.Write(initial);
+        while (sum < limit)
+        {
+            initial++;
+            sum += initial;
+            Console.Write(" + " + initial);
+        }
+        Console.Write(" = " + sum + " > " + limit);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("Please enter an integer number : ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter an integer number to limit : ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Function.Limit(a, b);
+    }
+}
